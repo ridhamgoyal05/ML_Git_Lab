7eda02638e5d119afa30e1252e89629b6fcdf8cc
Ridham Goyal <ridham.goyal05@gmail.com>
2026-02-19 00:31:00 +0530

    Implemented Decision Tree model

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import StandardScaler
@@ -20,7 +20,7 @@ X = scaler.fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train model
-model = LinearRegression()
+model = DecisionTreeRegressor()
 model.fit(X_train, y_train)
 
 # Predict
