ef13c7f5ab953562682f47a625428d50c41a5b43
Ridham Goyal <ridham.goyal05@gmail.com>
2026-02-19 00:27:49 +0530

    Added feature scaling

--- model.py
+++ model.py
@@ -2,12 +2,19 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
+from sklearn.preprocessing import StandardScaler
+
 
 # Load dataset
 data = pd.read_csv("dataset.csv")
 
 X = data[["size"]]
 y = data["price"]
+
+# Feature Scaling
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
 
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
